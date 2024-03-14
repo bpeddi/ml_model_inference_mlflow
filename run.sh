cad78c4980a44b63ed3293bd7f2575c4cd90a9f4
Bala Peddi <Bala.Peddi@gmail.com>
2024-03-14 01:31:45 +0000

    new code

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ source .venv/bin/activate
 pip install -r requirements.txt
 mlflow ui --port 8080 --backend-store-uri sqlite:///mlruns.db
 python mlmodel.py
-mlflow models build-docker -m models:/Diabetes_model/11 -n diabetes_model:latest --enable-mlserve
+#mlflow models build-docker -m models:/Diabetes_model/11 -n diabetes_model:latest --enable-mlserve
+mlflow models generate-dockerfile -m "models:/Diabetes_model/12"
